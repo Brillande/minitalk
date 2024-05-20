90da62c29d9b66562e65e45a1fe82ab6a91c11f4
brillande <emedina-@DESKTOP-GHPVO1T>
2024-05-21 00:04:45 +0200

    first commit

---
+++ inc/minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emedina- <emedina-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 16:05:27 by emedina-          #+#    #+#             */
+/*   Updated: 2024/04/26 16:10:20 by emedina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../printf/ft_printf.h"
+# include <signal.h>
+
+int	ft_atoi(const char *str);
+
+#endif
